5ae00506ac9a0c4b220128d1e722ed5550ba0be9
Samuel Schuyler <sjs08496@creighton.edu>
2021-10-28 13:05:27 -0500

    mobileNav

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -4,7 +4,7 @@ hmbMenu.onshow=function(){
 
 hmbMenu.onclick=function(s){ // when just click the control. "s" is
                               // the object returned
-    if (typeof(s) == "object") { // do nothing - they just clicked on the control
+    if (typeof(s) ==  "object") { // do nothing - they just clicked on the control
        return
     } else {
        switch(s) {
